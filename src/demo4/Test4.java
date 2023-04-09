f521a76fb7f1ad286a0d0eec93bfb2022aa2796b
valen <valen@DESKTOP-057E61O.lan>
2023-04-09 18:23:39 -0400

    new commit

---
+++ src/demo4/Test4.java
@@ -0,0 +1,15 @@
+package demo4;
+
+public class Test4 {
+	
+	public static void main(String arg ) {
+		
+		System.out.println("Test 4 ");
+	}
+	
+	public void newTry() {
+		
+		System.out.println("Test 4 ");
+	}
+
+}
